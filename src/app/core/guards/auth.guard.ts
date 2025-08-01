e0385306f2b6990fcd795b46c50d7245d08dbd4c
JaraPava <alberto-11111@live.com.mx>
2025-08-01 09:44:22 -0600

    feat: implementa guards para proteger rutas

---
+++ src/app/core/guards/auth.guard.ts
@@ -0,0 +1,27 @@
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+
+export const authGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (authService.isAuthenticated()) {
+    return true;
+  } else {
+    router.navigate(['/auth/login']);
+    return false;
+  }
+};
+
+export const loginGuard = () => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  if (!authService.isAuthenticated()) {
+    return true;
+  } else {
+    router.navigate(['/dashboard']);
+    return false;
+  }
+};
